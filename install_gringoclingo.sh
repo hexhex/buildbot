458745daca0c6dd84e614f4568e38f35337705a7
Peter Schüller <ps@kr.tuwien.ac.at>
2011-07-04 14:48:51 +0000

    added gringo/clingo installation script s.t. the result is usable for building dlvhex --with-libclingo

---
+++ install_gringoclingo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# getting args:
+# $1 = daemon directory
+# $2 = target directory
+
+# our task: copy structure and all .{h,hpp,a} and some binaries
+
+DDIR=$1/build/
+TDIR=$2/
+
+test -n "${DDIR}" || exit 1
+test -n "${TDIR}" || exit 1
+
+# dbg
+echo $DDIR
+echo $TDIR
+
+# clean target
+rm -rf $TDIR
+
+#set -x
+
+# sync {h,hpp,a} files
+rsync -rv \
+  --exclude=tests \
+  --exclude="*cmake*" --exclude="*Make*" \
+  --exclude=".svn" \
+  --exclude="*.cpp" --exclude="*.o" \
+  $DDIR $TDIR 
+
+# permissions for all users
+chmod -R ugo+X,ugo+r $TDIR
+chmod -R ugo+x $TDIR/build/release/bin/
+
+exit 0
